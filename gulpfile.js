960eb282512340a6ebbce136dc8370edd6076761
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-09 15:36:03 +0300

    Create minifyScripts Create bowerJs Create bowerCSS Create bower

--- gulpfile.js
+++ gulpfile.js
@@ -21,15 +21,45 @@ var lib = require('bower-files')({
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
-
+//task jshint
 gulp.task("jshint", function() {
   return gulp.src(["js/*.js"])
     .pipe(jshint())
     .pipe(jshint.reporter("default"));
 });
-
+//task concatInterface
 gulp.task("concatInterface", function() {
-  return gulp.src(["/js/*-interface.js"])
-  pipe(concat("allConcat.js"))
-  pipe(gulp.dest(".tmp"));
+  return gulp.src(["./js/*-interface.js"])
+    .pipe(concat("allConcat.js"))
+    .pipe(gulp.dest("./tmp"));
 });
+//task jsBrowserify
+gulp.task("jsBrowserify", ["concatInterface"], function() {
+  return browserify({
+      entries: ["./tmp/allConcat.js"]
+    })
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(gulp.dest("./build/js"));
+});
+//task minifyScripts
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src(["./build/js/app.js"])
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+//task bowerJS
+gulp.task("bowerJS", function() {
+  return gulp.src(lib.ext("js").files)
+    .pipe(concat("vendor.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"))
+});
+// task bowerCSS
+gulp.task("bowerCSS", function() {
+  return gulp.src(lib.ext("css").files)
+    .pipe(concat("vendor.css"))
+    .pipe(gulp.dest("./build/css"));
+});
+//task bower
+gulp.task("bower", ["bowerJS", "bowerCSS"]);
