054db1fc69f074b906f09c15ec8afa0652121f98
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-10 21:38:47 +0300

    Update the github-user.js Update the getGithubUser method and pass the github user in it Add the .get (github user) request Add the .get (get user's repositories) request Update the github-user-interface.js Call the getGithubUser method and pass the github user in it

--- build/js/app.js
+++ build/js/app.js
@@ -3,36 +3,54 @@ exports.apiKey = "b7fe71031b62aec5c8f6af3002bafe582dbeaeee";
 
 },{}],2:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
+//create GithubUser Object
+Githubuser = function() {
+
+};
+//Create constructor with method getGithubUser()
+Githubuser.prototype.getGithubUser = function(gitUser) {
+  // get github user
+  $.get("https://api.github.com/users/" + gitUser + "?access_token=" + apiKey, (function(results) {
+    // console.log(results.name);
+    // console.log(results.bio);
+    $('#get-user-repos').prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>");
+  }));
+  //get user's repositories
+  $.get("https://api.github.com/users/" + gitUser + "/repos" + "?access_token=" + apiKey, (function(responses) {
+    console.log(responses);
+    console.log(JSON.stringify(responses));
+    responses.forEach(function(response) {
+      if (response.description === null) {
+        $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + "Oooopss!! Repository Description not found !!!" + "</p></br>");
+      } else {
+        $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + response.description + "</p></br>");
+      }
+      // console.log(response.name);
+      // console.log(response.html_url);
+      // console.log(response.description);
+    });
+  }));
+}
+exports.GithubuserModule = Githubuser;
+
+},{"./../.env":1}],3:[function(require,module,exports){
+//require the github-user.js file here
+var Githubuser = require("./../js/github-user.js").GithubuserModule;
 
 $(document).ready(function() {
+  var currentGithubUserObject = new Githubuser();
+
   $('#userRepos').click(function(event) {
-    console.log("grooot");
+    // console.log("grooot");
     event.preventDefault();
+
     var gitUser = $('#gitUsername').val();
     $('#gitUsername').val(" ");
-    console.log(gitUser);
-    // get github user
-    $.get("https://api.github.com/users/" + gitUser + "?access_token=" + apiKey, (function(results) {
-      // console.log(results.name);
-      // console.log(results.bio);
-      $('#get-user-repos').prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>");
-    }));
-    //get user's repositories
-    $.get("https://api.github.com/users/" + gitUser + "/repos" + "?access_token=" + apiKey, (function(responses) {
-      console.log(responses);
-      console.log(JSON.stringify(responses));
-      responses.forEach(function(response) {
-        if (response.description === null) {
-          $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + "Oooopss!! Repository Description not found !!!" + "</p></br>");
-        } else {
-          $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + response.description + "</p></br>");
-        }
-        // console.log(response.name);
-        // console.log(response.html_url);
-        // console.log(response.description);
-      });
-    }));
+
+    // console.log(gitUser);
+
+    currentGithubUserObject.getGithubUser(gitUser);
   });
 });
 
-},{"./../.env":1}]},{},[2]);
+},{"./../js/github-user.js":2}]},{},[3]);
--- js/github-user.js
+++ js/github-user.js
@@ -1,10 +1,30 @@
 var apiKey = require('./../.env').apiKey;
 //create GithubUser Object
-function Githubuser {
+Githubuser = function() {
 
-}
+};
 //Create constructor with method getGithubUser()
-Githubuser.prototytpe.getGithubUser() {
-
+Githubuser.prototype.getGithubUser = function(gitUser) {
+  // get github user
+  $.get("https://api.github.com/users/" + gitUser + "?access_token=" + apiKey, (function(results) {
+    // console.log(results.name);
+    // console.log(results.bio);
+    $('#get-user-repos').prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>");
+  }));
+  //get user's repositories
+  $.get("https://api.github.com/users/" + gitUser + "/repos" + "?access_token=" + apiKey, (function(responses) {
+    console.log(responses);
+    console.log(JSON.stringify(responses));
+    responses.forEach(function(response) {
+      if (response.description === null) {
+        $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + "Oooopss!! Repository Description not found !!!" + "</p></br>");
+      } else {
+        $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + response.description + "</p></br>");
+      }
+      // console.log(response.name);
+      // console.log(response.html_url);
+      // console.log(response.description);
+    });
+  }));
 }
 exports.GithubuserModule = Githubuser;
