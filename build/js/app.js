45116dc09f2302ae098453f8c7ea3afd9ca4ee17
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-10 22:39:49 +0300

    Display the number of public repositories

--- build/js/app.js
+++ build/js/app.js
@@ -3,20 +3,21 @@ exports.apiKey = "b7fe71031b62aec5c8f6af3002bafe582dbeaeee";
 
 },{}],2:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
+
 //create GithubUser Object
 Githubuser = function() {
 
 };
+
 //Create constructor with method getGithubUser()
 Githubuser.prototype.getGithubUser = function(gitUser) {
   // get github user
   $.get("https://api.github.com/users/" + gitUser + "?access_token=" + apiKey).then(function(results) {
-    // console.log(results.name);
-    // console.log(results.bio);
-    $("#get-user-repos").prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>");
+    $("#get-user-repos").prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>" + "<p>" + "Public Repositories: " + results.public_repos + "</p></br>");
   }).fail(function(error) {
     $("#get-user-repos").text(error.responseJSON.message);
   });
+
   //get user's repositories
   $.get("https://api.github.com/users/" + gitUser + "/repos?access_token=" + apiKey).then(function(responses) {
     console.log(responses);
@@ -27,14 +28,12 @@ Githubuser.prototype.getGithubUser = function(gitUser) {
       } else {
         $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + response.description + "</p></br>");
       }
-      // console.log(response.name);
-      // console.log(response.html_url);
-      // console.log(response.description);
     });
   }).fail(function(error) {
     $("#get-user-repos").text(error.responseJSON.message);
   });
 };
+
 exports.GithubuserModule = Githubuser;
 
 },{"./../.env":1}],3:[function(require,module,exports){
--- js/github-user.js
+++ js/github-user.js
@@ -1,18 +1,19 @@
 var apiKey = require('./../.env').apiKey;
+
 //create GithubUser Object
 Githubuser = function() {
 
 };
+
 //Create constructor with method getGithubUser()
 Githubuser.prototype.getGithubUser = function(gitUser) {
   // get github user
   $.get("https://api.github.com/users/" + gitUser + "?access_token=" + apiKey).then(function(results) {
-    // console.log(results.name);
-    // console.log(results.bio);
-    $("#get-user-repos").prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>");
+    $("#get-user-repos").prepend("<p>" + "Github User: " + results.name + "</p></br>" + "<p>" + "Github User Status: " + results.bio + "</p></br>" + "<p>" + "Github username: " + gitUser + "</p></br>" + "<p>" + "Public Repositories: " + results.public_repos + "</p></br>");
   }).fail(function(error) {
     $("#get-user-repos").text(error.responseJSON.message);
   });
+
   //get user's repositories
   $.get("https://api.github.com/users/" + gitUser + "/repos?access_token=" + apiKey).then(function(responses) {
     console.log(responses);
@@ -23,12 +24,10 @@ Githubuser.prototype.getGithubUser = function(gitUser) {
       } else {
         $("#get-user-repos").append("<ol>" + response.name + "</ol>" + "<p>" + response.html_url + "</p>" + "<p>" + response.description + "</p></br>");
       }
-      // console.log(response.name);
-      // console.log(response.html_url);
-      // console.log(response.description);
     });
   }).fail(function(error) {
     $("#get-user-repos").text(error.responseJSON.message);
   });
 };
+
 exports.GithubuserModule = Githubuser;
